cfc0be5bb1f0c2f4280ea8a05e02ea9eefecfced
tarmesh <sharmalucky21450@gmail.com>
2026-02-19 19:54:08 +0530

    half convertion operations

---
+++ 01_basics/03_convertionOperations.js
@@ -0,0 +1,37 @@
+let score = "hitesh"
+
+// console.log(typeof score); //string
+// console.log(typeof(score)); 
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"  => 33
+// "33abc" => NaN
+// "abc33" => NaN
+// "abc" => NaN
+// " " => 0
+// "" => 0
+// true => 1 ; false => 0
+
+
+let isLoggedIn = "hitesh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+
+// 1 => true
+// 0 => false
+// "hitesh" => true
+// "" => false
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber); // "33"
+console.log(typeof stringNumber); //string
+
